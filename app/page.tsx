3dde0e33eea83c1bdcc579f0264e80a701057f2e
Milenko Ilic <milenko.ilic997@gmail.com>
2026-05-22 13:30:17 +0200

    Adding sections to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import Hero from "@/components/hero";
+import Work from "@/components/work";
+import About from "@/components/about";
+import Process from "@/components/process";
+import Services from "@/components/services";
+import Contact from "@/components/contact";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="main-content">
-        <h1 className="flex py-100 text-align-center justify-center text-3xl">Add your components intead this line</h1>
+      <main>
+        <Hero />
+        <Work />
+        <About />
+        <Process />
+        <Services />
+        <Contact />
       </main>
       <Footer />
     </>
